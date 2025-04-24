baad0885f5862226323750bb9e2a7f308024db12
Mario H. Tornería Ramírez <mh.torneria@gmail.com>
2025-04-24 17:06:29 -0400

    ?

--- frontend-bitacora-ifolab/src/app/dashboard/layout.js
+++ frontend-bitacora-ifolab/src/app/dashboard/layout.js
@@ -2,7 +2,6 @@ import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 export default async function DashboardLayout({ children }) {
-  // Obtiene las cookies del servidor
   const cookieStore = await cookies()
   const token = cookieStore.get('token')?.value
   
